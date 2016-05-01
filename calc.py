92b8dafac27c03b49f5728df9f35a079ba5acc9a
duzitong <duzitong811@126.com>
2016-05-01 12:50:38 +0800

    remove some duplicate

--- calc.py
+++ calc.py
@@ -11,12 +11,20 @@ def operate(a, b, gate_type):
         return a ^ b
 
 def gate_output_generator(line):
-  for i in range(len(line)):
-    for j in range(i+1, len(line)):
-      for gate_type in range(5):
-        yield operate(line[i], line[j], gate_type) 
+    for i in range(len(line)):
+        for j in range(i+1, len(line)):
+            for gate_type in range(5):
+                yield operate(line[i], line[j], gate_type) 
+
+def check(i, line):
+    if i in line:
+        return False
+    if i == 0:
+        return False
+    return True
 
 def operate_line(line):
-  output = set(gate_output_generator(line))
-  for i in output:
-    yield sorted(line + [i])
+    output = set(gate_output_generator(line))
+    for i in output:
+        if check(i, line):
+            yield sorted(line + [i])
